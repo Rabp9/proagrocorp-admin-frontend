5002ae0fb49ed733f6374598a153daef89ff88fa
rabp_ <rabp_@192.168.1.9>
2018-10-15 00:10:04 -0500

    avanzando

--- app/scripts/controllers/users-login.js
+++ app/scripts/controllers/users-login.js
@@ -8,10 +8,26 @@
  * Controller of the proagrocorpAdminFrontendApp
  */
 angular.module('proagrocorpAdminFrontendApp')
-  .controller('UsersLoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('UsersLoginCtrl', function ($scope, usersService, $cookies, $location, 
+    $rootScope, $utilsViewService) {
+    
+    $scope.loginUser = function(user, boton) {
+        $('#' + boton).text('Login...');
+        $utilsViewService.disable('#' + boton);
+        
+        usersService.login(user, function(data) {
+            if (!data.user) {
+                $scope.message = data.message;
+            } else {
+                $cookies.putObject('globalagro-user', data.user);
+                $cookies.put('globalagro-token', data.token);
+                $rootScope.user = data.user;
+                $('#wrapper').removeClass('inLogin');
+                $location.path('/');
+            }
+        }, function(error) {
+            $scope.message = error.data;
+        });
+    };
+    
+});
--- app/scripts/services/usersservice.js
+++ app/scripts/services/usersservice.js
@@ -8,16 +8,19 @@
  * Factory in the proagrocorpAdminFrontendApp.
  */
 angular.module('proagrocorpAdminFrontendApp')
-  .factory('usersService', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+.factory('usersService', function ($resource, envService) {
+    return $resource(envService.getHost() + 'users/:id.json', {}, {
+        getPersonas: {
+            method: 'GET',
+            url: envService.getHost() + 'users/getPersonas/.json'
+        },
+        login: {
+            method: 'POST',
+            url: envService.getHost() + 'users/token/.json',
+        },
+        getAdmin: {
+            method: 'GET',
+            url: envService.getHost() + 'users/getAdmin/.json',
+        }
+    });
+});
