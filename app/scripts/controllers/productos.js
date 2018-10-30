55f63ed98475d94e0e7189303015d1ad4fc2818d
tmtrbocanegra <tmtrbocanegra@SGGT-PC.TMT.LOCAL>
2018-10-30 16:50:45 -0500

    Avanzando

--- app/scripts/controllers/productos.js
+++ app/scripts/controllers/productos.js
@@ -8,10 +8,36 @@
  * Controller of the proagrocorpAdminFrontendApp
  */
 angular.module('proagrocorpAdminFrontendApp')
-  .controller('ProductosCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('ProductosCtrl', function ($scope, productosService, $utilsViewService) {
+    $scope.search = {};
+    $scope.search.estado_id = '1';
+    
+    $scope.getProductos = function() {
+        $scope.loading = true;
+        productosService.getAdmin(function(data) {
+            $scope.productos = data.productos;
+            $scope.loading = false;
+        });
+    };
+    
+    $scope.init = function() {
+        $scope.getUsers();
+    };
+    
+    $scope.showUsersAdd = function(event) {
+        $utilsViewService.disable(event.currentTarget);
+        
+        var modalInstanceAdd = $uibModal.open({
+            templateUrl: 'views/users-add.html',
+            controller: 'UsersAddCtrl',
+            backdrop: false
+        });
+        
+        $utilsViewService.enable(event.currentTarget);
+        
+        modalInstanceAdd.result.then(function (data) {
+            $scope.getUsers();
+            $scope.message = data;
+        });
+    };
+});
